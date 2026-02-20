8ad7f05811c3c17019e45c0f913d03863b9534a3
Javier Figueroa <javierfigueroa969@gmail.com>
2026-02-20 18:47:41 +0100

    estructurar carpetas backend + empezar analisis datos

--- backend/main.py
+++ backend/main.py
@@ -21,5 +21,9 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
