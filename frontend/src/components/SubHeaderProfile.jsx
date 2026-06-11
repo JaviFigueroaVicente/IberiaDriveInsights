8e40e101b958e88c2136af156090d7804a55acae
Javier Figueroa <javierfigueroa969@gmail.com>
2026-06-11 02:51:11 +0200

    corregir error al importar en SubHeaderProfile

--- frontend/src/components/SubHeaderProfile.jsx
+++ frontend/src/components/SubHeaderProfile.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import Menu from "../assets/icons/Menu.svg";
+import Menu from "../assets/icons/menu.svg";
 
 export default function SubHeaderProfile() {
     const location = useLocation();
